c44977f5bb673f3bccda4aa12ba28667d69b8524
Sergy Stepanov <sergystepanov@gmail.com>
2017-01-21 20:57:37 +0300

    Add time/month interface elements

--- script/script.js
+++ script/script.js
@@ -1,12 +1,29 @@
 "use strict";
 
+
+
 function startClock() {
+	var time = document.getElementById('clock-value'),
+	day = document.getElementById('month-day'),
+	month = document.getElementById('month-name'),
+	year = document.getElementById('year');
+
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     m = checkTime(m);
-    document.getElementById('clock-value').innerHTML = h + ":" + m;
+    time.innerHTML = h + ":" + m;
+
+	day.innerHTML = today.getDate();
+	var monthNames = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сеп", "Окт", "Нов", "Дек"];
+	month.innerHTML = monthNames[today.getMonth()];
+	year.innerHTML = today.getFullYear();
+
     var t = setTimeout(startClock, 500);
+};
+
+var updateDate = function(date) {
+
 };
 
 function checkTime(i) {
